a91a2ca75d8d068601f4ebc4bee02ad628cde4d3
Ramanathan <ramanathan@mrama.local>
2018-02-22 14:55:15 +0530

    added notification click event

--- public/sw.js
+++ public/sw.js
@@ -22,3 +22,19 @@ self.addEventListener('push', (event) => {
     );
   }
 });
+
+self.addEventListener('notificationclick', event => {
+  const rootUrl = new URL('/', location).href;
+  event.notification.close();
+  // Enumerate windows, and call window.focus(), or open a new one.
+  event.waitUntil(
+    clients.matchAll().then(matchedClients => {
+      for (let client of matchedClients) {
+        if (client.url === rootUrl) {
+          return client.focus();
+        }
+      }
+      return clients.openWindow("/");
+    })
+  );
+});
